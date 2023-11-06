5eb88d8104faa52a8f1dbc9b2dc766428c1f2499
eduardo1700 <edu.alex.mp@gmail.com>
2023-11-06 17:43:36 +0000

    fim projeto

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,85 +1,93 @@
-import React, { useState } from 'react'
-import "./WeatherApp.css"
+import React, { useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import searchIcon from "../Images/search.png"
-import clear from "../Images/clear.png"
-import cloud from "../Images/cloud.png"
-import drizzle from "../Images/drizzle.png"
-import rain from "../Images/rain.png"
-import snow from "../Images/snow.png"
-import wind from "../Images/wind.png"
-import humidity from "../Images/humidity.png"
-export default function WeatherApp() {
+import "./WeatherApp.css";
+import searchIcon from "../Images/search.png";
+import clear from "../Images/clear.png";
+import cloud from "../Images/cloud.png";
+import drizzle from "../Images/drizzle.png";
+import rain from "../Images/rain.png";
+import snow from "../Images/snow.png";
+import wind from "../Images/wind.png";
+import humidity from "../Images/humidity.png";
 
+export default function WeatherApp() {
   let api_key = "f3aab99dfa38bd3f721442d874b59f72";
-  const[wicon,setWicon]= useState(cloud)
+  const [wicon, setWicon] = useState(cloud);
 
   const search = async () => {
-    const element = document.getElementsByClassName("cityInput")
+    const element = document.getElementsByClassName("cityInput");
     if (element[0].value === "") {
-      return 0
+      return 0;
     }
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
-    
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
     let response = await fetch(url);
+
     if (response.status === 404) {
       toast.error('City not found', {
-        position: "top-right",
+        position: toast.POSITION.TOP_CENTER,
         autoClose: 3000,
       });
       return;
     }
+
     let data = await response.json();
-    const humidity = document.getElementsByClassName("humidity-percent")
-    const wind = document.getElementsByClassName("wind-rate")
-    const temp = document.getElementsByClassName("weather-temp")
-    const location = document.getElementsByClassName("weather-location")
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temp = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
     humidity[0].innerHTML = `${data.main.humidity} %`;
     wind[0].innerHTML = `${Math.floor(data.wind.speed)} km/h`;
     temp[0].innerHTML = `${Math.floor(data.main.temp)}ºc`;
-    location[0].innerHTML = data.name
+    location[0].innerHTML = data.name;
 
-    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n"){
-      setWicon(clear)
-    } else if (data.weather[0].icon === "02d" || data.weather[0].icon === "02n"){
-      setWicon(cloud)
-    } else if (data.weather[0].icon === "03d" || data.weather[0].icon === "03n"){
-      setWicon(drizzle)
-    }else if (data.weather[0].icon === "04d" || data.weather[0].icon === "04n") {
-    setWicon(drizzle)
+    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+      setWicon(clear);
+    } else if (data.weather[0].icon === "02d" || data.weather[0].icon === "02n") {
+      setWicon(cloud);
+    } else if (data.weather[0].icon === "03d" || data.weather[0].icon === "03n") {
+      setWicon(drizzle);
+    } else if (data.weather[0].icon === "04d" || data.weather[0].icon === "04n") {
+      setWicon(drizzle);
     } else if (data.weather[0].icon === "09d" || data.weather[0].icon === "09n") {
-      setWicon(rain)
+      setWicon(rain);
     } else if (data.weather[0].icon === "10d" || data.weather[0].icon === "10n") {
-      setWicon(rain)
+      setWicon(rain);
     } else if (data.weather[0].icon === "13d" || data.weather[0].icon === "13n") {
-      setWicon(snow)
-    }else{
-      setWicon(clear)
+      setWicon(snow);
+    } else {
+      setWicon(clear);
     }
-
   }
+
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       search();
     }
   };
+
   return (
     <div className="container">
-      <div className='top-bar'>
-        <input type="text" className='cityInput' placeholder='Search' onKeyDown={handleKeyPress} />
-        <div className='search-icon' onClick={() => { search() }}>
-          <img src={searchIcon} />
+      <div className="top-bar">
+        <input
+          type="text"
+          className="cityInput"
+          placeholder="Search"
+          onKeyDown={handleKeyPress}
+        />
+        <div className="search-icon" onClick={search}>
+          <img src={searchIcon} alt="Search" />
         </div>
       </div>
       <div className="weather-image">
         <img src={wicon} alt="" />
       </div>
-      <div className="weather-temp">24ºc </div>
+      <div className="weather-temp">24ºc</div>
       <div className="weather-location">London</div>
-      <div className='data-container'>
+      <div className="data-container">
         <div className="element">
           <img src={humidity} alt="" className="icon" />
           <div className="data">
@@ -95,8 +103,7 @@ export default function WeatherApp() {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
-
-
-  )
+  );
 }
